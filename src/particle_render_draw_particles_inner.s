3b2a2bae6b9320f0e24fae4ae69a5242e10596f4
Jonathan Thomas <jonathan@opalise.co.uk>
2022-04-04 22:22:00 +0100

    particles optimise

--- src/particle_render_draw_particles_inner.s
+++ src/particle_render_draw_particles_inner.s
@@ -29,9 +29,7 @@ _particle_render_draw_particles_inner:
     move.l 12(a0),a2 ; a2 = current particle draw pointer
     move.l 20(a0),a4 ; a4 = ypos pointers
     move.l 16(a0),a0 ; a0 = hardware_playfield_buffer
-    move.l a0,usp
     move.w _logical_viewport_left_xpos,d4 ; d4 = logical viewport left xpos
-    ;lea _hardware_playfield_ypos_lookup,a4
     lea _hardware_viewport_xpos_lookup,a5
     lea _or_table,a0
     lea _or_table_mask_lookup,a6
@@ -46,55 +44,39 @@ _particle_render_draw_particles_inner:
     beq.s .alldone
 
 .handle_particle:
-    ; logical_viewport_particle_ypos (d2) =
-    ; current_particle->precision_world_xpos >> 16 (because of +2)
+    ; get current_particle->precision_world_xpos >> 16 (because of address +2)
+    ; if above top of screen, move to next particle
     move.w PRECISION_WORLD_YPOS_OFS(a3),d2
-    blt.s .next_particle ; if above top of screen, move to next particle
+    blt.s .next_particle
 
     ; get current_particle->precision_world_xpos >> 16 (because of address +2)
     move.w (a3),d5
 
     ; adjust xpos to location within hardware viewport
-    sub.w d4,d5 ; logical_viewport_particle_xpos -= logical_viewport_left_xpos
+    sub.w d4,d5
 
-    ; get the offset within the hardware viewport representing the particle ypos
-    ; (multiply ypos by 480)
-    ; if I have one of these for each buffer, can I put absolute pointers in them
-    ; and get rid of the a1 add below?
+    ; get the pointer to the start of the buffer line where the particle
+    ; needs to be drawn
     add.w d2,d2
     add.w d2,d2
-    move.l (a4,d2.w),d2 ; get value in hardware_playfield_ypos_lookup
+    move.l (a4,d2.w),a1
 
-    ; get the offset within the hardware viewport representing the adjusted xpos
+    ; add the offset within the hardware viewport representing the adjusted xpos
     add.w d5,d5
-    move.w (a5,d5.w),d6
+    add.w (a5,d5.w),a1
 
-    ; add the xpos offset to the ypos offset
-    add.l d6,d2
-
-    move.l d2,a1
-
-    ; get the absolute offset in memory where we need to start drawing the particle
-    ; I think I can get rid of this!
-    ;move.l usp,a1
-    ;add.l d2,a1
-
-    ; a1 now contains hardware_playfield_particle_ptr
-    ; hardware_viewport_particle_xpos << 1 is preserved in d5
-
-    ; derive or_table_mask_offsets
-    move.w (a6,d5.w),d2 ; or_table_mask_lookup[hardware_viewport_particle_xpos]
-
+    ; derive offset within or table
+    move.w (a6,d5.w),d2
     move.w TIME_TO_LIVE_OFS(a3),d5 ; time to live: 0 - 63
 
-    ; this is the movep plot pixel code
-    movep.l 0(a1),d1  ; get pixel
-    and.l (a0,d2.w),d1   ; apply AND
+    ; plot the pixel
+    movep.l 0(a1),d1
+    and.l (a0,d2.w),d1
     add.b .exhaust_trail_colours(pc,d5.w),d2
-    or.l (a0,d2.w),d1   ; apply OR in WHITE
-    movep.l d1,0(a1)  ; write back
+    or.l (a0,d2.w),d1
+    movep.l d1,0(a1)
 
-    ; now do the current particle draw pointer
+    ; store pointer to write location to erase later
     move.l a1,(a2)+
 
     addq.l #1,d0     ; inc number of particles drawn
